3e583c3ddadf588bcd572f7534a910f08ab6e066
Oleg <Oleg-BLR@yandex.ru>
2023-01-10 18:27:48 +0300

    Solution 07

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,41 +1,19 @@
 package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ListSorter {
 
-    public static void main(String[] args) {
-//        List<String> initialList = new ArrayList<>(Arrays.asList("-5", "-12", "0", "20", "9", "-20", "37"));
-        List<String> initialList = new ArrayList<>(Arrays.asList("7", "10", "6", "-7", "-2", "0", "-2", "-23"));
-        ListSorter listSorter = new ListSorter();
-//        System.out.println(listSorter.sort(initialList));
-        System.out.println(listSorter.sort(initialList));
-    }
 
-//    public void sort(List<String> sourceList) {
-    public List<String> sort(List<String> sourceList) {
-//        ArrayList<Element> listOfElements = new ArrayList<>();
-//        ArrayList<String> initialList = new ArrayList<>();
-//        System.out.println(fillModifiedStringList(fillElementsWithArrayList(sourceList)));
+    public void sort(List<String> sourceList) {
         List<Element> elements = fillElementsWithArrayList(sourceList);
         fillModifiedStringList(elements, sourceList);
-        return sourceList;
     }
-
-//    public List<String> sort(List<String> sourceList) {
-////        ArrayList<Element> listOfElements = new ArrayList<>();
-////        ArrayList<String> initialList = new ArrayList<>();
-////        System.out.println(fillModifiedStringList(fillElementsWithArrayList(sourceList)));
-//        return fillModifiedStringList(fillElementsWithArrayList(sourceList));
-//    }
-
 
     private List<Element> fillElementsWithArrayList(List<String> initialListData) {
         List<Element> arrayListWithElements = new ArrayList<>();
-//        int i = 0;
         for (String stringElement : initialListData) {
             Element element = new Element();
             element.setInitialStringValue(stringElement);
@@ -51,10 +29,6 @@ public class ListSorter {
     private List<String> fillModifiedStringList(List<Element> arrayListWithElements, List<String> sourceList) {
         sourceList.clear();
         arrayListWithElements.forEach(element -> sourceList.add(0, element.initialStringValue));
-//        List<String> modifiedStringList = new ArrayList<>();
-//        for (Element element : arrayListWithElements) {
-//            modifiedStringList.add(0, element.initialStringValue);
-//        }
         return sourceList;
     }
 }
